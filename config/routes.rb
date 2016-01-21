039d0a25ee362fc22f521843fdb96863136943e4
chengdh <cheng.dongh@gmail.com>
2016-01-21 22:58:54 +0800

    登录界面出来了

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations" }
 
-  resources :register_infos do
-    get :intro,on: :collection
-  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'register_infos#index'
+  #root 'register_infos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/migrate/20150504152511_add_user_id_to_register_info.rb
+++
@@ -1,6 +0,0 @@
-#coding: utf-8
-class AddUserIdToRegisterInfo < ActiveRecord::Migration
-  def change
-    add_column :register_infos, :user_id, :integer
-  end
-end
