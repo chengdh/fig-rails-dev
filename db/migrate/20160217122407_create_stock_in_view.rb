3892067fcc773f86a8db6d1bfa5d2b73de0b5fe6
chengdh <cheng.dongh@gmail.com>
2016-03-15 23:22:55 +0800

    fixed create stock view bug

--- db/migrate/20160217122407_create_stock_in_view.rb
+++ db/migrate/20160217122407_create_stock_in_view.rb
@@ -3,9 +3,9 @@
 class CreateStockInView < ActiveRecord::Migration
   def change
     execute "CREATE OR REPLACE VIEW v_stock_in AS " +
-      "SELECT t_location_id,equipment_id,sum(qty) qty,sum(qty*price) amt FROM inout_lines group by t_location_id,equipment_id WHERE state='confirmed'"
+      " SELECT t_location_id,equipment_id,sum(qty) qty,sum(qty*price) amt FROM inout_lines WHERE state='confirmed' group by t_location_id,equipment_id"
     execute "CREATE OR REPLACE VIEW v_stock_out AS " +
-      "SELECT f_location_id,equipment_id,sum(qty) qty,sum(qty*price) amt FROM inout_lines group by f_location_id,equipment_id WHERE state='confirmed'"
+      " SELECT f_location_id,equipment_id,sum(qty) qty,sum(qty*price) amt FROM inout_lines WHERE state='confirmed' group by f_location_id,equipment_id "
     execute "CREATE OR REPLACE VIEW v_stock AS " +
     " SELECT v_stock_in.t_location_id location_id,v_stock_in.equipment_id,(IFNULL(v_stock_in.qty,0) -IFNULL(v_stock_out.qty,0)) qty,"+
     " NOW() created_at," +
