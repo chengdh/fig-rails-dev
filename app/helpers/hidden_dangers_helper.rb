f6adbb1de37a3ea5e050e434d13748034c5d41c8
chengdh <cheng.dongh@gmail.com>
2016-03-15 21:28:39 +0800

    修改了devise.scss的错误

--- app/helpers/hidden_dangers_helper.rb
+++ app/helpers/hidden_dangers_helper.rb
@@ -3,4 +3,10 @@ module HiddenDangersHelper
   def fix_periods_for_select
     [["15日",15],["1个月",30],["3个月",90],["6个月",180]]
   end
+  def fix_states_for_select
+    [["草稿(未处理)","draft"],
+     ["已下发","deliveried"],
+     ["处理中","processing"],
+     ["整改完成","fixed"]]
+  end
 end
