fb00793a010878a54b914beb592b518e397660da
chengdh <cheng.dongh@gmail.com>
2016-03-24 13:18:43 +0800

    修改了安全月报表的bug

--- app/models/safety_table.rb
+++ app/models/safety_table.rb
@@ -6,15 +6,16 @@ class SafetyTable < ActiveRecord::Base
 
   #整改率
   def fixed_danger_rate
+    return 0 if danger_count.to_f == 0
     (fixed_danger_count.to_f/danger_count.to_f).round(3)*100
   end
 
   #安保总投入
   def sum_invest
-    invest_equipment_money + invest_faclities_money + fixed_danger_money + safety_edu_money + safety_reward_money
+    invest_equipment_money.to_i + invest_faclities_money.to_i + fixed_danger_money.to_i + safety_edu_money.to_i + safety_reward_money.to_i
   end
 
   def sum_safety_check_count
-    safety_check_count+special_safety_check_count + evening_safety_check_count 
+    safety_check_count.to_i+special_safety_check_count.to_i + evening_safety_check_count.to_i
   end
 end
