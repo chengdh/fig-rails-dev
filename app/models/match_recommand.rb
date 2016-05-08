a01f5f2785cd8f1881bcd094e566b9faaa6b61b7
chengdh <cheng.dongh@gmail.com>
2016-05-08 10:57:40 +0800

    修改了胜率计算不正确的问题

--- app/models/match_recommand.rb
+++ app/models/match_recommand.rb
@@ -17,7 +17,7 @@ class MatchRecommand < ActiveRecord::Base
     sum_win_half = q.result.where("result_type = 1 ").where(recommend_type: recommend_type).sum(0.5)
     sum_matches = q.result.where("result_type <> 0 ").where(recommend_type: recommend_type).sum(1)
     win_rate = 0
-    win_rate = ((sum_win_all + sum_win_half)/sum_matches*100).round(1) if sum_matches > 0
+    win_rate = ((sum_win_all + sum_win_half).to_f/sum_matches.to_f*100).round(1) if sum_matches > 0
     win_rate
   end
 end
