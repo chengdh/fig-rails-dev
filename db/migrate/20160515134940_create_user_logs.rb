64e72e43eda94b343621bafde8ca209166f92744
chengdh <cheng.dongh@gmail.com>
2016-05-16 20:24:12 +0800

    change user_log index

--- db/migrate/20160515134940_create_user_logs.rb
+++ db/migrate/20160515134940_create_user_logs.rb
@@ -2,7 +2,7 @@
 class CreateUserLogs < ActiveRecord::Migration
   def change
     create_table :user_logs do |t|
-      t.references :user
+      t.references :user, index: true
       t.string :op_type,limit: 30
 
       t.timestamps null: false
