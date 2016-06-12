c236172ce2cd9052bcac43f1f713ba526dd76996
chengdh <cheng.dongh@gmail.com>
2016-06-12 22:36:59 +0800

    战绩统计接口添加了分页功能

--- app/controllers/match_recommands_controller.rb
+++ app/controllers/match_recommands_controller.rb
@@ -18,7 +18,7 @@ class MatchRecommandsController < ApplicationController
   #GET /report_interface.json
   def report_interface
     @q = MatchRecommand.where("result_type IS NOT NULL").ransack(params[:q])
-    @match_recommands = @q.result
+    @match_recommands = @q.result.paginate(page: params[:page], per_page: params[:per_page])
 
     @yinglang_match_recommands = @q.result.where(recommend_type: 2)
     @bigdata_match_recommands = @q.result.where(recommend_type: 1)
