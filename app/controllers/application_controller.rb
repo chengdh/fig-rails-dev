c2bd2c4293041b680519f4153f865cd5f45845ba
chengdh <cheng.dongh@gmail.com>
2016-08-06 16:45:03 +0800

    修改了因devise升级导致的登录错误

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  #参考http://stackoverflow.com/questions/38215274/devise-nomethoderror-for-parametersanitizer
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :user_name, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:user_name, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:user_name, :email, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in,keys: [:login, :user_name, :email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update,keys: [:user_name, :email, :password, :password_confirmation, :current_password])
   end
 end
