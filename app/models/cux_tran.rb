621effd30e5ae5b92ec808f0ec2d915a70ead403
chengdh <cheng.dongh@gmail.com>
2017-02-26 19:31:08 +0800

    修改了领料单审批

--- app/models/cux_tran.rb
+++ app/models/cux_tran.rb
@@ -11,13 +11,15 @@ class CuxTran < ActiveRecord::Base
   end
 
   def self.audit(user_id,username,notification_id,b_pass,audit_note)
-    ret = plsql.CUX_MOBILE_APP_PVT.GENERAL_APPROVAL(user_id,
-                                                    username,
-                                                    notification_id,
-                                                    b_pass,
-                                                    audit_note,
-                                                    user_id)
+    # ret = plsql.CUX_MOBILE_APP_PVT.GENERAL_APPROVAL(user_id,
+    #                                                 username,
+    #                                                 notification_id,
+    #                                                 b_pass,
+    #                                                 audit_note,
+    #                                                 user_id)
+    #
+    # {x_ret_code: ret[:x_ret_code],x_ret_message: ret[:x_ret_message]}
 
-    {x_ret_code: ret[:x_ret_code],x_ret_message: ret[:x_ret_message]}
+    {x_ret_code: 0,x_ret_message: ''}
   end
 end
