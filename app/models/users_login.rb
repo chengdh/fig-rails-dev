324787666c2848ee816b9d704a021150aa6ad51e
chengdh <cheng.dongh@gmail.com>
2017-06-01 12:10:52 +0800

    添加了maximo数据同步功能

--- app/models/users_login.rb
+++ app/models/users_login.rb
@@ -5,4 +5,8 @@ class UsersLogin < ActiveRecord::Base
     user_ids = pluck(:user_id)
     user_ids.each {|u_id| WfNotification.sync_with_ebs(u_id)}
   end
+  def self.sync_with_maximo
+    user_names = pluck(:username)
+    user_names.each {|u_name| MaximoMsg.sync_with_maximo(u_name)}
+  end
 end
